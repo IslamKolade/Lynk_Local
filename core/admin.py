e3c519b26d43d762c8b4848f37a11dadfcc725a3
IslamKolade <islamsanni66@gmail.com>
2023-09-15 17:35:13 +0100

    Added Admin Site Header, Title and Index Title.

--- core/admin.py
+++ core/admin.py
@@ -3,3 +3,8 @@ from .models import LynkLocal, Contact_Form_Submission
 # Register your models here.
 admin.site.register(LynkLocal)
 admin.site.register(Contact_Form_Submission)
+
+admin.site.site_header = 'LynkLocal Admin'
+admin.site.site_title = 'LynkLocal Admin Panel'
+admin.site.index_title = 'Welcome to LynkLocal Admin'
+
