fd29415fde32a18f80ec52abe862873f25efe724
IslamKolade <islamsanni66@gmail.com>
2023-09-15 10:57:09 +0100

    first commit

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import LynkLocal, Contact_Form_Submission
+# Register your models here.
+admin.site.register(LynkLocal)
+admin.site.register(Contact_Form_Submission)
