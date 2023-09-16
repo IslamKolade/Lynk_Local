6071533c8422241ec9cec2cf583cd39a0b0a506b
IslamKolade <islamsanni66@gmail.com>
2023-09-16 02:48:25 +0100

    Added Filter to Contact Models and Corrected LynkLocal Description

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import LynkLocal, Contact_Form_Submission
 # Register your models here.
+class Contact_Filter(admin.ModelAdmin): 
+    search_fields = ('name',)
+    list_filter = ('userType','registrationOption')
+
+
 admin.site.register(LynkLocal)
-admin.site.register(Contact_Form_Submission)
+admin.site.register(Contact_Form_Submission, Contact_Filter)
 
 admin.site.site_header = 'LynkLocal Admin'
 admin.site.site_title = 'LynkLocal Admin Panel'
